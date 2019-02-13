daafc7c283deb2e11f34ded6baef0ae994b3222f
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-02-13 14:29:23 -0500

    Optimisation and refactoring

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,36 +1,20 @@
-function isMedia(URL) {
+function isMedia(origin) {
     chrome.storage.local.get(['media_urls'], function(result) {
         var websites = JSON.parse(result.media_urls).media_urls;
-        var resultcount = 0;
-        for (index in websites) {
-            // console.log(websites[index][0])
-            if (URL.indexOf(index) > -1) {
-                var fact = websites[index][0]
-                console.log(fact)
-                resultcount = resultcount + 1;
-            }
-        }
-        if (resultcount > 0) {
-            console.log("Hello. This message was sent from scripts/inject.js");
-            inject(document.URL, fact)
+        var website = websites[origin][0];
+        if (origin !== void 0) {
+            inject(website);
         }
     });    
 }
 
-chrome.extension.sendMessage({}, function (response) {
-    window.addEventListener('load', function () {
-        // ----------------------------------------------------------
-        // This part of the script triggers when page is done loading
-        //alert("Hello. This message was sent from scripts/inject.js");
-        // ----------------------------------------------------------
 
-        isMedia(document.URL);
-    });
+window.addEventListener('load', function () {
+    isMedia(location.host.split(".")[0] == "www" ? location.host.replace(/www\./g, "") : location.host);
 });
 
-function inject(url, mediafacts) {
-    var htmlString = '<div id="betterweb-injected-banner"><div id="betterweb-injected-banner-content">'+'Bias: '+mediafacts.bias+', factual: '+mediafacts.factual+'</div></div>'
 
-    //parsedDOM = new DOMParser().parseFromString(htmlString, "text/xml");
+function inject(mediafacts) {
+    var htmlString = '<div id="betterweb-injected-banner"><div id="betterweb-injected-banner-content">'+'Bias: '+mediafacts.bias+', factual: '+mediafacts.factual+'</div></div>'
     document.body.insertAdjacentHTML('beforeend', htmlString)
 }
