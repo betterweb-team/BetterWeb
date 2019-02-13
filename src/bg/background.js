72420c8589301f55717b95d3604f138c7fe1f44c
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-02-13 12:31:21 -0500

    No minification plz

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,10 +1,3 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -14,16 +7,13 @@ chrome.extension.onMessage.addListener(
 
 chrome.runtime.onInstalled.addListener(function(details){
   if(details.reason == "install"){
-      // TODO move media_urls.json to LocalStorage
-      const media_urls = chrome.runtime.getURL('src/shared/json/media_urls.json.min')
+      const media_urls = chrome.runtime.getURL('src/shared/json/media_urls.json')
       fetch(media_urls)
         .then(function (response) {
             return response.json();
         })
         .then(function (json) {
-            //localStorage.setItem('media_urls', JSON.stringify(json));
             chrome.storage.local.set({'media_urls': JSON.stringify(json)});
         });
-
   }
 });
