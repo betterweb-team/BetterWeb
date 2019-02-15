0a1fcca2612e132b38065acd37d61614a8a3c535
Haoji Xu <haoji.xu@outlook.com>
2019-02-15 15:37:01 -0500

    changed regex pattern

--- src/inject/js/inject.js
+++ src/inject/js/inject.js
@@ -1,3 +1,11 @@
+function injectAnalytics () {
+    const script = document.createElement('script');
+    script.setAttribute("type", "module");
+    script.setAttribute("src", chrome.extension.getURL('src/inject/js/article.js'));
+    const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+    head.insertBefore(script, head.lastChild);
+}
+
 function getBiasData(origin) {
     return new Promise(function(resolve) {
         chrome.storage.local.get(['media_urls'], function(result) {
@@ -9,16 +17,19 @@ function getBiasData(origin) {
 }
 
 function inject(mediafacts) {
-    var htmlString = '<div id="betterweb-injected-banner"><div id="betterweb-injected-banner-content">'+'Bias: '+mediafacts.bias+', factual: '+mediafacts.factual+'</div></div>'
+    var htmlString = `<div id="betterweb-injected-banner"><div id="betterweb-injected-banner-content">'+'Bias: '+mediafacts.bias+', factual: '+mediafacts.factual+'</div></div>`
     document.body.insertAdjacentHTML('beforeend', htmlString)
 }
 
 chrome.extension.sendMessage({}, function() { // DOM is not available without this call fsr
     window.addEventListener('load', async function() {
-        var biasData = await getBiasData(location.hostname.replace(/^www\./, ""));
+        var biasData = await getBiasData(location.hostname.match(/[^.]*\.[^.]{2,3}(?:\.[^.]{2,3})?$/, "")[0]);
+        console.log(biasData)
         var settingsData = await retrieve.getStoredSettings(["extension_enabled", "media_bias_enabled"])
         if (biasData !== void 0 && settingsData.extension_enabled != "false" && settingsData.media_bias_enabled != "false") {
             inject(biasData[0]);
         }
     });
 })
+
+//injectAnalytics();
