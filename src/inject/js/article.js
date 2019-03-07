3c058a4ddbd8d928ea4262b3c5a07d00ebd8fe3f
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-03-07 11:11:02 -0500

    Optimise hostName matching by moving logic to page_action.js

--- src/inject/js/article.js
+++ src/inject/js/article.js
@@ -1,6 +1,8 @@
 // Mercury content extraction page
+/*
 window.addEventListener('load', function() {
     Mercury.parse(document.URL).then(result => {
         console.log(result);
     });
 });
+*/
