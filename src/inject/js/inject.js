0dd21f879fd0b3859b2db3883601c71da23e844e
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-03-07 11:11:02 -0500

    Optimise hostName matching by moving logic to page_action.js

--- src/inject/js/inject.js
+++ src/inject/js/inject.js
@@ -1,26 +1,13 @@
-function getBiasData(origin) {
-    return new Promise(function(resolve) {
-        chrome.storage.local.get(['media_urls'], function(result) {
-            var websites = JSON.parse(result.media_urls).media_urls;
-            var website = websites[origin]; //This is an array with [0] as data!
-            resolve(website);
-        });
-    });
-}
+var pageData = {};
+pageData.hostName = location.hostname.match(/[^.]*\.[^.]{2,3}(?:\.[^.]{2,3})?$/, "")[0]
 
-(async function main() {
-    var biasData = await getBiasData(location.hostname.match(/[^.]*\.[^.]{2,3}(?:\.[^.]{2,3})?$/, "")[0])
+chrome.runtime.sendMessage({
+    from: 'content',
+    subject: 'showPageAction'
+});
 
-    chrome.runtime.sendMessage({
-        from: 'content',
-        subject: 'showPageAction'
-    });
-
-    chrome.runtime.onMessage.addListener(function(msg, sender, response) {
-        if ((msg.from === 'popup') && (msg.subject === 'getData') && biasData) {
-            response({ data: biasData[0], match: true });
-        } else {
-            response({ match: false });
-        }
-    });
-})()
+chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+    if ((msg.from === 'popup') && (msg.subject === 'getData')) {
+        response(pageData);
+    }
+});
--- src/page_action/js/page_action.js
+++ src/page_action/js/page_action.js
@@ -1,6 +1,23 @@
-function renderPopup(data) {
-    if (!data.match) { return }
-    document.querySelector("html").innerHTML = data.data.bias + " " + data.data.factual;
+function getBiasData(hostName) {
+    return new Promise(function(resolve) {
+        chrome.storage.local.get(['media_urls'], function(result) {
+            var websites = JSON.parse(result.media_urls).media_urls;
+            var website = websites[hostName]; //This is an array with [0] as data!
+            if (website === void 0) { // Page not listed 
+                resolve(void 0)
+            } else {
+                resolve(website[0]);
+            }
+        });
+    });
+}
+
+async function renderPopup(data) {
+    var biasData = await getBiasData(data.hostName);
+    if (biasData) {
+        document.querySelector("html").innerHTML = biasData.bias + " " + biasData.factual;
+    }
+
 }
 
 window.addEventListener('DOMContentLoaded', function() {
